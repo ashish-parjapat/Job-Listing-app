1986af17a1cc77c0a8e3c5be49f80450fa7b8c53
Ashish Kumar <ashish.kalyan007@gmail.com>
2023-04-13 17:34:41 +0530

    added database config and initialzation

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express')
+const mongoose=require('mongoose')
 const app = express()
 const env=require('dotenv')
 env.config()
 const port = process.env.PORT||3000
+//mongdb connection 
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log("connected to DB successfuly");
+}).catch((e)=>{
+    console.log(e);
+})
+
+
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
